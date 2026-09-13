49f72826f3f6a191e7b8033470851cc5dbcb2ce7
Drheann <nesokheandr007@gmail.com>
2026-09-13 23:16:36 +0700

    Fix Google login - add account selection prompt

--- app/Http/Controllers/Auth/GoogleAuthController.php
+++ app/Http/Controllers/Auth/GoogleAuthController.php
@@ -21,6 +21,7 @@ class GoogleAuthController extends Controller
 
         return Socialite::driver('google')
             ->stateless()
+            ->prompt('select_account')
             ->redirect();
     }
 
